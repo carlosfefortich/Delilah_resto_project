1b62f1b3840426fa84735aadb1a4b519a705c4d8
Carlos Fortich <carlosfefortich@gmail.com>
2021-02-03 01:08:13 -0500

    Fixed an error on role validation

--- controllers/users/createUser.js
+++ controllers/users/createUser.js
@@ -2,11 +2,11 @@ const {User} = require('../../db');
 
 const createUser = async (req, res)=> {
     const newUser = req.body;
-    if(newUser.role != 'admin' || newUser.role != 'customer'){
-        res.status(400).json({error: 'role property must be admin or customer'});
-    }else{
+    if(newUser.role === 'admin' || newUser.role === 'customer'){
         const user = await User.create(newUser);
         res.status(200).json({message: 'User created successfully', user: user});
+    }else{
+        res.status(400).json({error: 'role property must be admin or customer'});
     }
     
 }
