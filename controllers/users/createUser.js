b748fa7d164a76289107da4547f45666a4efd45b
Carlos Fortich <carlosfefortich@gmail.com>
2021-02-03 00:53:52 -0500

    Validation on roles added

--- controllers/users/createUser.js
+++ controllers/users/createUser.js
@@ -2,8 +2,13 @@ const {User} = require('../../db');
 
 const createUser = async (req, res)=> {
     const newUser = req.body;
-    const user = await User.create(newUser);
-    res.status(200).json({message: 'User created successfully', user: user});
+    if(newUser.role != 'admin' || newUser.role != 'customer'){
+        res.status(400).json({error: 'role property must be admin or customer'});
+    }else{
+        const user = await User.create(newUser);
+        res.status(200).json({message: 'User created successfully', user: user});
+    }
+    
 }
 
 module.exports = {createUser};
